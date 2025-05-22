c01c4dad5886faf25defc6a0a6707820dd7db803
Tayse Rosa <tayse_rosa@yahoo.com.br>
2025-05-22 16:26:08 -0300

    Begining...

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import AddTask from "./components/AddTask";
+import Tasks from "./components/Tasks";
+
+function App() {
+  const [tasks, setTasks] = useState([
+    { id: 1, title: "Estudar programaçãp", isCompleted: false },
+    { id: 2, title: "Estudar inglês", isCompleted: true },
+    { id: 3, title: "Estudar Matemática", isCompleted: false },
+  ]);
+
+  return (
+    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+      <div className="w-[500px]">
+        <h1 className="text-3xl text-slate-100 font-bold text-center">
+          Gerenciador de tarefas
+        </h1>
+        <AddTask />
+        <Tasks tasks={tasks} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
